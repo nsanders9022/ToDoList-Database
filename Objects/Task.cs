c37ec890db2a6f61a0bbbadb3b380e63dff88156
Brandon Cooper and Nicole Sanders <brandon+nicole@>
2017-02-20 09:26:22 -0800

    add code to Startup.cs and project.json to include sql databases; add a Database.cs Objects file and class; create tests to test Find, Save, empty, same methods;

--- Objects/Task.cs
+++ Objects/Task.cs
@@ -1,4 +1,9 @@
+using Xunit;
 using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using ToDoList.Objects;
 
 namespace ToDoList.Objects
 {
@@ -6,15 +11,26 @@ namespace ToDoList.Objects
   {
     private string _description;
     private int _id;
-    private static List<Task> _instances = new List<Task>{};
 
-    public Task (string description)
+    public Task (string description, int Id = 0)
     {
       _description = description;
-      _instances.Add(this);
-      _id = _instances.Count;
+      _id = Id;
     }
-
+    public override bool Equals(System.Object otherTask)
+    {
+      if (!(otherTask is Task))
+      {
+        return false;
+      }
+      else
+      {
+        Task newTask = (Task) otherTask;
+        bool idEquality = (this.GetId() == newTask.GetId());
+        bool descriptionEquality = (this.GetDescription() == newTask.GetDescription());
+        return (idEquality && descriptionEquality);
+      }
+    }
     public string GetDescription()
     {
       return _description;
@@ -24,21 +40,114 @@ namespace ToDoList.Objects
     {
       _description = newDescription;
     }
-    public static List<Task> GetAll()
-    {
-      return _instances;
-    }
     public int GetId()
     {
       return _id;
     }
-    public static void ClearAll()
+    // public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
+    // public static Task Find(int searchId)
+    // {
+    //   return _instances[searchId - 1];
+    // }
+    public static void DeleteAll()
     {
-      _instances.Clear();
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM tasks;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
     }
-    public static Task Find(int searchId)
+    public static List<Task> GetAll()
     {
-      return _instances[searchId - 1];
+      List<Task> allTasks = new List<Task>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int taskId = rdr.GetInt32(0);
+        string taskDescription = rdr.GetString(1);
+        Task newTask = new Task(taskDescription, taskId);
+        allTasks.Add(newTask);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allTasks;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO tasks (description) OUTPUT INSERTED.id VALUES (@TaskDescription);", conn);
+
+      SqlParameter descriptionParameter = new SqlParameter();
+      descriptionParameter.ParameterName = "@TaskDescription";
+      descriptionParameter.Value = this.GetDescription();
+      cmd.Parameters.Add(descriptionParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public static Task Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks WHERE id = @TaskId;", conn);
+      SqlParameter taskIdParameter = new SqlParameter();
+      taskIdParameter.ParameterName = "@TaskId";
+      taskIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(taskIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int foundTaskId = 0;
+      string foundTaskDescription = null;
+      while(rdr.Read())
+      {
+        foundTaskId = rdr.GetInt32(0);
+        foundTaskDescription = rdr.GetString(1);
+      }
+      Task foundTask = new Task(foundTaskDescription, foundTaskId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return foundTask;
     }
   }
 }
--- Startup.cs
+++ Startup.cs
@@ -38,4 +38,8 @@ namespace ToDoList
       get { return false; }
     }
   }
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo;Integrated Security=SSPI;";
+  }
 }
